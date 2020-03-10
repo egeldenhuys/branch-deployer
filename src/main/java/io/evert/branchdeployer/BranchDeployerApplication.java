d1f8e91fb7c1132d755b1d3006a21756881dab8e
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2020-03-10 17:18:42 +0200

    Bootstrap application

---
+++ src/main/java/io/evert/branchdeployer/BranchDeployerApplication.java
@@ -0,0 +1,13 @@
+package io.evert.branchdeployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchDeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchDeployerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/evert/branchdeployer/BranchDeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.evert.branchdeployer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchDeployerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
